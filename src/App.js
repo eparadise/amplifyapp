77594167f325ccf01cd6477b18401a0683f706ca
Eliza Paradise <eliparad1@publicisgroupe.net>
2022-04-22 11:56:55 -0400

    added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+import { Amplify } from 'aws-amplify';
 
-function App() {
+Amplify.configure(awsExports);
+
+function App({signOut, user}) {
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hi, {user.username}, We now have Auth!</h1>
       </header>
+      <button onClick={signOut}>Sign out</button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
