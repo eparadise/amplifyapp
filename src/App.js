40da02bee2e84f507f97f2f61c28c69e9a087760
Eliza Paradise <eliparad1@publicisgroupe.net>
2022-04-22 13:14:26 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports';
-import { Amplify } from 'aws-amplify';
-
-Amplify.configure(awsExports);
 
 function App({signOut, user}) {
 
